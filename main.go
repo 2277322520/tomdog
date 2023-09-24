9cb5d88ce0a6787a4a2078561ebb8dc56346eb82
Setsuna Yang <99322388+3546514206@users.noreply.github.com>
2023-09-24 23:08:37 +0800

    兼容windows的语法

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import "tomdog/tdnet"
 
 func main() {
 	server := tdnet.NewServer()
-	
+
 	server.AddRouter(&tdnet.PingRouter{
 		BaseRouter: tdnet.BaseRouter{RouterId: 10001},
 	})
 	server.AddRouter(&tdnet.TestRouter{
 		BaseRouter: tdnet.BaseRouter{RouterId: 10002},
 	})
-	
+
 	server.Serve()
 }
