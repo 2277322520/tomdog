8ffd89c8582df41c428aa27c90a19d3ebc47dcb5
杨海波 <3546514206@qq.com>
2023-09-25 22:14:18 +0800

    提交

--- test/data_pack_client_test.go
+++ test/data_pack_client_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 	"testing"
+	"time"
 	"tomdog/tdnet"
+	"tomdog/utils"
 )
 
 func TestDataPackClient(t *testing.T) {
@@ -13,6 +15,26 @@ func TestDataPackClient(t *testing.T) {
 		fmt.Println("client dial err:", err)
 	}
 
+	doRequest(err, conn)
+
+	doneChannel := make(chan bool)
+
+	go ParseResponse(doneChannel, conn)
+
+	// 客户端阻塞
+	select {
+	case done := <-doneChannel:
+		// 处理接收到的数据
+		if done {
+			utils.Logging("client 001 got response!!!")
+		}
+	case <-time.After(5 * time.Second):
+		// 超时处理，五秒钟内其他分支没有管道返回则认为请求超时
+		utils.Logging("client 001 time out!!!")
+	}
+}
+
+func doRequest(err error, conn net.Conn) bool {
 	// 创建一个封包对象
 	dp := tdnet.NewDataPack()
 	message_1 := &tdnet.Message{
@@ -24,7 +46,7 @@ func TestDataPackClient(t *testing.T) {
 	sendMessage_1, err := dp.Pack(message_1)
 	if err != nil {
 		fmt.Println("pack message err,", sendMessage_1)
-		return
+		return true
 	}
 
 	message_2 := &tdnet.Message{
@@ -36,7 +58,7 @@ func TestDataPackClient(t *testing.T) {
 	sendMessage_2, err := dp.Pack(message_2)
 	if err != nil {
 		fmt.Println("pack message err,", sendMessage_2)
-		return
+		return true
 	}
 
 	data := append(sendMessage_1, sendMessage_2...)
@@ -45,11 +67,5 @@ func TestDataPackClient(t *testing.T) {
 	if err != nil {
 		fmt.Println("write data err:", data)
 	}
-
-	doneChannel := make(chan bool)
-
-	go ParseResponse(doneChannel, conn)
-
-	// 客户端阻塞
-	select {}
+	return false
 }
